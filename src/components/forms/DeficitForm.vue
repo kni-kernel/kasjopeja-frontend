d147b5172197ed919a75fc9dd73526ed6856df1a
Paulina Kulczak <58978325+p-kulczak@users.noreply.github.com>
2020-06-03 21:40:37 +0200

    [KASJ-34]  Create page with re-attending subjects (#6)
    * Basic component structure
    
    * Add BasicDataForm
    
    * Add welcome page
    
    * Fix background image
    
    * [KASJ-37] Create page with dismissal from certain form of study
    
    Co-authored-by: Piotr Moszkowicz <piotr@moszkowicz.pl>
    Co-authored-by: klaudiaf24 <klaudia.fil@gmail.com>

--- src/components/forms/DeficitForm.vue
+++ src/components/forms/DeficitForm.vue
@@ -1,14 +1,183 @@
 <template>
-  <h1>Deficit form</h1>
+  <mu-container>
+    <mu-form
+      :model="form"
+      class="form"
+      label-position="right"
+      label-width="200"
+    >
+      <mu-form-item
+        prop="subjects"
+        class="container"
+        label="Przedmiot z bazy WFiIS"
+      >
+        <mu-select v-model="form.subjects">
+          <mu-option
+            v-for="option in subjects"
+            :key="option"
+            :label="option"
+            :value="option"
+          ></mu-option>
+        </mu-select>
+      </mu-form-item>
+      <mu-button color="primary" class="button" @click="handleButtonClick">
+        Dodaj wybrany przedmiot
+      </mu-button>
+      <br /><br />
+      <mu-button color="primary" class="button" @click="showModal">
+        Dodaj przedmiot spoza bazy WFiIS
+      </mu-button>
+      <modal v-show="isModalVisible" @close="closeModal" />
+      <mu-container
+        :class="{ highlighted: isTableVisible === false }"
+        id="table"
+      >
+        <mu-paper>
+          <mu-data-table
+            v-if="list.length > 0"
+            :columns="columns"
+            max-height="200"
+            :data="list"
+          >
+            <template slot-scope="scope">
+              <td>{{ scope.row.name }}</td>
+              <td>{{ scope.row.session }}</td>
+              <td>{{ scope.row.ects }}</td>
+              <td>{{ scope.row.faculty }}</td>
+              <td>
+                <button class="btn-close" @click="deleteItem(scope.row)">
+                  X
+                </button>
+              </td>
+            </template>
+          </mu-data-table>
+        </mu-paper>
+      </mu-container>
+    </mu-form>
+  </mu-container>
 </template>
 
 <script>
+import modal from "./ExternalSubjectModal.vue";
 export default {
   name: "DeficitForm",
+  components: {
+    modal
+  },
   props: {
     formValues: Object
+  },
+  data() {
+    return {
+      subjects: ["Programowanie Obiektowe I", "Labfiz"],
+      form: {
+        subjects: ""
+      },
+      isModalVisible: false,
+      isTableVisible: true,
+      sort: {
+        name: "",
+        order: "asc"
+      },
+      columns: [
+        { title: "Przedmiot", width: 400, name: "name", align: "center" },
+        { title: "Semestr", name: "session", width: 100, align: "center" },
+        { title: "ECTS", name: "ects", width: 100, align: "center" },
+        { title: "Wydział", name: "faculty", width: 100, align: "center" },
+        { title: "Usuń", name: "deleteItem", width: 100, align: "center" }
+      ],
+      list: [
+        {
+          name: "Programowanie obiektowe",
+          session: 1,
+          ects: 5,
+          faculty: "WFiIS"
+        },
+        {
+          name: "Programowanie obiektowe 2",
+          session: 4,
+          ects: 5,
+          faculty: "WFiIS"
+        },
+        {
+          name: "Programowanie obiektowe 4",
+          session: 3,
+          ects: 5,
+          faculty: "WEAIB"
+        },
+        {
+          name: "Programowanie obiektowe",
+          session: 1,
+          ects: 5,
+          faculty: "WFiIS"
+        },
+        {
+          name: "Programowanie obiektowe 2",
+          session: 4,
+          ects: 5,
+          faculty: "WFiIS"
+        },
+        {
+          name: "Programowanie obiektowe 4",
+          session: 3,
+          ects: 5,
+          faculty: "WEAIB"
+        }
+      ]
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+      this.isTableVisible = false;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+      this.isTableVisible = true;
+    },
+    deleteItem(item) {
+      const index = this.list.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.list.splice(index, 1);
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form {
+  margin-top: 3em;
+  width: 100%;
+  max-width: 600px;
+}
+.container {
+  position: relative;
+}
+.button {
+  position: absolute;
+  right: 10vw;
+  width: 300px;
+}
+#table.container {
+  margin: 50px;
+  width: max-content;
+  /*z-index: 0;*/
+  padding: 20px;
+}
+td {
+  text-align: center;
+}
+.highlighted {
+  z-index: -2;
+}
+.btn-close {
+  border: none;
+  font-size: 14px;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  font-weight: bold;
+  color: #777777;
+  background: transparent;
+}
+</style>
---
+++ src/components/forms/ExternalSubjectModal.vue
@@ -0,0 +1,159 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            Przedmiot spoza bazy WFiIS:
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              X
+            </button>
+          </slot>
+        </header>
+
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            <mu-form-item prop="subject" label="Nazwa przedmiotu">
+              <mu-text-field v-model="form.subject" type="text"></mu-text-field>
+            </mu-form-item>
+            <mu-form-item prop="hours" label="Liczba godzin W/C/L/P/S/I:">
+              <mu-text-field v-model="form.numberOfHours" type="text"></mu-text-field>
+            </mu-form-item>
+            <mu-form-item prop="ects" label="Ilość punktów ECTS:">
+              <mu-text-field v-model="form.ectsPoints" type="text"></mu-text-field>
+            </mu-form-item>
+            <mu-form-item prop="faculty" label="Wydział:">
+              <mu-select v-model="form.faculty">
+                <mu-option
+                  v-for="option in departments"
+                  :key="option"
+                  :label="option"
+                  :value="option"
+                ></mu-option>
+              </mu-select>
+            </mu-form-item>
+            <mu-form-item prop="session" label="Semestr:">
+              <mu-select v-model="form.session">
+                <mu-option
+                  v-for="option in sessions"
+                  :key="option"
+                  :label="option"
+                  :value="option"
+                ></mu-option>
+              </mu-select>
+            </mu-form-item>
+          </slot>
+        </section>
+
+        <footer class="modal-footer">
+          <slot name="footer">
+            <mu-button
+              type="button"
+              color="primary"
+              class="button"
+              @click="close"
+              aria-label="Close modal"
+            >
+              Dodaj
+            </mu-button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "ExternalSubjectModal",
+  props: {
+    formValues: Object
+  },
+  data() {
+    return {
+      departments: ["WFiIS", "WGiG", "WIMiIP", "WEAIiIB", "WIEiT", "WIMiR", "WGGiOŚ", "WGGiIŚ", "WIMiC", "WO", "WMN", "WWNiG", "WZ", "WEiP", "WMS", "WH"],
+      sessions: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"],
+      form: {
+        subject: "",
+        hours: "",
+        ects: "",
+        faculty: "",
+        session: ""
+        // payload = {'key1': 'value1', 'key2': 'value2'}
+      }
+    };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  color: #009688;
+  justify-content: space-between;
+  margin: 15px;
+  font-size: 1.5em;
+}
+
+.modal-footer {
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 5px 25px 5px 10px;
+  width: 500px;
+}
+
+.btn-close {
+  border: none;
+  font-size: 20px;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  font-weight: bold;
+  color: #009688;
+  background: transparent;
+}
+
+</style>
