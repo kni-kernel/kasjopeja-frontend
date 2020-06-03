cf9f3c5e90ac63da3815ab1ccfe67e5fa7486d32
Paulina Kulczak <58978325+p-kulczak@users.noreply.github.com>
2020-06-03 22:01:41 +0200

    [KASJ-29] Create required subjects page (#10)
    * Basic component structure
    
    * Add BasicDataForm
    
    * Add welcome page
    
    * Fix background image
    
    * [KASJ-37] Create page with dismissal from certain form of study
    
    * KASJ-33  Create page with subjects completed in previous semesters
    
    * [KASJ-31] Create optional subject page
    
    * [KASJ-32] Create additional subject page
    
    * [KASJ-29] Create required subject page
    
    Co-authored-by: Piotr Moszkowicz <piotr@moszkowicz.pl>
    Co-authored-by: klaudiaf24 <klaudia.fil@gmail.com>

--- src/components/forms/ObligatorySubjectsForm.vue
+++ src/components/forms/ObligatorySubjectsForm.vue
@@ -1,14 +1,82 @@
 <template>
-  <h1>Obligatory Subjects form</h1>
+  <mu-container :class="{ highlighted: isTableVisible === false }" id="table">
+    <dataTable
+      :list="list"
+      :columns="columns"
+      :deleteButton="false"
+      :checkBox="true"
+    />
+  </mu-container>
 </template>
 
 <script>
+import dataTable from "./DataTableTemplate.vue";
+
 export default {
   name: "ObligatorySubjectsForm",
+  components: {
+    dataTable
+  },
   props: {
     formValues: Object
+  },
+  data() {
+    return {
+      isTableVisible: true,
+      sort: {
+        name: "",
+        order: "asc"
+      },
+      columns: [
+        { title: "Przedmiot", width: 400, name: "name", align: "center" },
+        { title: "Liczba godzin", name: "hours", width: 200, align: "center" },
+        { title: "ECTS", name: "ects", width: 100, align: "center" },
+        { title: "Dopisz do planu", name: "checkBox", width: 150, align: "center" }
+      ],
+      list: [
+        {
+          name: "Programowanie obiektowe",
+          hours: "30/0/30/0/0/0",
+          ects: 5,
+          checkBox: true
+        },
+        {
+          name: "Programowanie obiektowe",
+          hours: "30/0/30/0/0/0",
+          ects: 1,
+          checkBox: true
+        },
+        {
+          name: "Programowanie obiektowe 2",
+          hours: "30/15/15/0/0/0",
+          ects: 1,
+          checkBox: true
+        },
+        {
+          name: "Programowanie obiektowe 4",
+          hours: "30/15/15/0/0/0",
+          ects: 5,
+          checkBox: true
+        },
+        {
+          name: "Programowanie obiektowe 2",
+          hours: "30/15/15/0/0/0",
+          ects: 5,
+          checkBox: true
+        }
+      ]
+    };
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#table {
+  margin: 50px;
+  width: max-content;
+  padding: 20px;
+}
+td {
+  text-align: center;
+}
+</style>
