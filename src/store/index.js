e9af2f96caa48ce2fbe94a1366f9dee1fe661f8e
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-06-14 00:16:54 +0200

    Add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import form from "./form.module";
+import subjects from "./subjects.module";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    form,
+    subjects
+  }
 });
